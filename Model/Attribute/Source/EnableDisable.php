af8aa7f908443615b62b89dd1cc053bb4bd0f916
Marcin Frymark <m.frymark@alekseon.com>
2021-10-27 18:03:48 +0200

    101.0.5

---
+++ Model/Attribute/Source/EnableDisable.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright © Alekseon sp. z o.o.
+ * http://www.alekseon.com/
+ */
+namespace Alekseon\AlekseonEav\Model\Attribute\Source;
+
+/**
+ * Class EnableDisable
+ * @package Alekseon\AlekseonEav\Model\Attribute\Source
+ */
+class EnableDisable extends AbstractSource
+{
+    const VALUE_DISABLE = 0;
+    const VALUE_ENABLE = 1;
+
+    /**
+     * @return array|mixed
+     */
+    public function getOptions()
+    {
+        return [
+            self::VALUE_DISABLE => __('Disabled'),
+            self::VALUE_ENABLE => __('Enabled'),
+        ];
+    }
+}
